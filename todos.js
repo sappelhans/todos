5b5fd6e582445f8580999117bc542b74dbb5cde6
sappelhans <Steve@Stevens-MacBook-Pro.local>
2015-12-29 12:34:58 -0600

    Simple Application Part 1
    Changes after the first video.

--- todos.js
+++ todos.js
@@ -1,23 +1,12 @@
+Todos = new Meteor.Collection('todos');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.TodosPanel.helpers({
+    items: function() {
+      return Todos.find();
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
